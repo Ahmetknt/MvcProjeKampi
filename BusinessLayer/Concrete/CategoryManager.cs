8bfa6794d8f3699b1873aa7c515096f8ed6b6917
AHMET KANAT <ahmtknt46@gmail.com>
2021-05-11 18:12:55 +0300

    30.Ders Sonu

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -1,4 +1,6 @@
-﻿using DataAccessLayer.Concrete.Repositories;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Repositories;
 using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
@@ -8,23 +10,29 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    public class CategoryManager
+    public class CategoryManager : ICategoryService
     {
-        GenericRepository<Category> repo = new GenericRepository<Category>();
+        ICategoryDal _categoryDal;
 
-        public List<Category> GetAllBL()
+        public CategoryManager(ICategoryDal categoryDal)
         {
-            return repo.List();
+            _categoryDal = categoryDal;
         }
+
+        public List<Category> GetList()
+        {
+            return _categoryDal.List();
+        }
+
         public void CategoryAddBL(Category p)
         {
-            if (p.CategoryName=="" || p.CategoryName.Length<=3 || p.CategoryDescription=="" || p.CategoryName.Length>=51)
+            if(p.CategoryName=="" || p.CategoryStatus==false || p.CategoryName.Length <= 2)
             {
                 //Hata Mesajı
             }
             else
             {
-                repo.Insert(p); 
+                _categoryDal.Insert(p);
             }
         }
     }
---
+++ DataAccessLayer/Migrations/202105111106172_mig2.cs
@@ -0,0 +1,20 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig2 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Abouts", "AboutImage1", c => c.String(maxLength: 100));
+            AlterColumn("dbo.Abouts", "AboutImage2", c => c.String(maxLength: 100));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Abouts", "AboutImage2", c => c.String(maxLength: 500));
+            AlterColumn("dbo.Abouts", "AboutImage1", c => c.String(maxLength: 500));
+        }
+    }
+}
