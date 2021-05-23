f9a2cf23d6686089e7e89795618b9bcb2a1591de
AHMET KANAT <ahmtknt46@gmail.com>
2021-05-23 12:58:25 +0300

    52.Ders Sonu

---
+++ DataAccessLayer/Migrations/202105230914541_writer_status_insert.cs
@@ -0,0 +1,18 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class writer_status_insert : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Writers", "WriterStatus", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Writers", "WriterStatus");
+        }
+    }
+}
